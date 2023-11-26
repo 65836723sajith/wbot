021c161e66a653e6640c0eda3c741339c0f7e1cb
65836723sajith <124520853+65836723sajith@users.noreply.github.com>
2023-11-27 00:20:26 +0530

    Update whatsapp_bot.js

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
 const venom = require('venom');
 const qrcode = require('qrcode-terminal');
 
-venom
-  .create({
-    headless: true, // Keep this as false if you want to see the browser
-    logQR: true, // Show the QR code in the terminal
-  })
-  .then((client) => {
-    console.log('WhatsApp bot is running!');
+venom.create().then((client) => {
+  console.log('WhatsApp bot is running!');
 
-    client.onMessage(async (message) => {
-      try {
-        const sender = message.from;
-        const receivedMessage = message.body.toLowerCase();
+  client.onMessage(async (message) => {
+    try {
+      const sender = message.from;
+      const receivedMessage = message.body.toLowerCase();
 
-        // Check for a specific trigger message
-        if (receivedMessage === 'hello') {
-          // Reply with a custom message
-          await client.sendText(sender, 'Hello! This is a WhatsApp bot.');
-        }
-      } catch (error) {
-        console.error('Error occurred:', error);
+      // Check for a specific trigger message
+      if (receivedMessage === 'hello') {
+        // Reply with a custom message
+        await client.sendText(sender, 'Hello! This is a WhatsApp bot.');
       }
-    });
-
-    // Display QR code in terminal
-    client.onStateChange((state) => {
-      if (state === 'QR_RECEIVED') {
-        qrcode.generate(client.qrCode, { small: true });
-      }
-    });
-  })
-  .catch((error) => {
-    console.error('Error creating WhatsApp bot:', error);
+    } catch (error) {
+      console.error('Error occurred:', error);
+    }
   });
+
+  // Display QR code in terminal
+  client.onStateChange((state) => {
+    if (state === 'QR_RECEIVED') {
+      console.log('Scan the QR code to link your WhatsApp account:');
+      qrcode.generate(client.qrCode, { small: true });
+    }
+  });
+}).catch((error) => {
+  console.error('Error creating WhatsApp bot:', error);
+});
